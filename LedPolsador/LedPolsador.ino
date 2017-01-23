64058025515a2a132ec7606c0e3144611af85ad3
MohamedMarnissi2017 <melmarnissi19962000@gmail.com>
2017-01-23 11:25:02 +0100

    LedPolsador
    Programa 3

---
+++ LedPolsador/LedPolsador.ino
@@ -0,0 +1,44 @@
+/****************************************************************
+**                                                             **
+**                          TÍTOL:                             **
+**                          BLINK                              **
+**                                                             **
+**  NOM: MOHAMED EL MARNISSI                  DATA: 23/01/2017 **
+****************************************************************/
+//************************ INCLUDE ****************************** 
+
+
+
+//*********************** VARIABLES *****************************
+
+
+
+//************************** SETUP ******************************
+
+void setup() {                //Inicia el bloc
+// inicia pin 13 com a sortida:
+  pinMode(13, OUTPUT);
+// initialize inicia pin 4 com a entrada:
+  pinMode(4, INPUT);
+
+}                             //Finalitza el bloc
+
+//*************************** LOOP ******************************
+
+void loop() {                 // inicia el bucle del programa
+ // Llegeix estat entrades:
+  buttonState = digitalRead(4);
+
+  // Comprova estat polsador.
+  // SI polsador apretat:
+  if (buttonState =digitalRead(4)= HIGH) {
+    // Encen LED:
+    digitalWrite(13, HIGH);
+  } else {
+    // En qualsevol altre cas apaga LED:
+    digitalWrite(13, LOW);
+  }
+
+}                          //Finalitza el bloc
+
+//************************** FUNCIONS ***************************
